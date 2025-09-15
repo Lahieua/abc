1ed31e4114bc476341c972a1dd06710065a8e4f1
Lahieua <136452993+Lahieua@users.noreply.github.com>
2025-09-15 21:46:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '*[ N O W - A V A I L A B L E - WA BETA ON PLAYSTORE ]*
+
+```https://play.google.com/apps/testing/com.whatsapp/join?hl=en-US```' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
